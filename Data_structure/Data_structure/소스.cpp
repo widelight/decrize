8c8e9f5d147243614a8b8688a0aece3e903751b8
widelight <kgch0301@gmail.com>
2022-04-14 10:59:24 +0900

    Update 소스.cpp
    deleteNode를 추가

--- Data_structure/Data_structure/소스.cpp
+++ Data_structure/Data_structure/소스.cpp
@@ -1,109 +1,114 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-class DblNode {
-private:
-	string* data;
-public:
-	DblNode* llink;
-	DblNode* rlink;
-	DblNode();
-	DblNode(string data);
-	string getData();
-	void setData(string data);
-
-};
-
-DblNode::DblNode() {
-	data = NULL;
-	llink = NULL;
-	rlink = NULL;
-}
-DblNode::DblNode(string data) {
-	*(this->data) = data;
-	llink = NULL;
-	rlink = NULL;
-}
-string DblNode::getData() {
-	return *data;
-}
-void DblNode::setData(string data) {
-	*(this->data) = data;
-}
+typedef struct Node{
+	string data;
+	Node* llink;
+	Node* rlink;
+	
+	Node() : llink(NULL), rlink(NULL), data(NULL) {};
+	Node(string data) : llink(NULL), rlink(NULL), data(data){};
+	};
 //mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 
 class DblLinkedList {
 private:
-	DblNode* head;
+	Node* head;
 public:
 	DblLinkedList();
-	DblNode* getHead();	
-	void insertNode(DblLinkedList* L, DblNode* pre, string data);
-	void deleteNode(DblLinkedList* L, DblNode* old);
+	~DblLinkedList();
+	Node* getHead();
+	void setHead(Node* willHead);
+	void insertNode(DblLinkedList* L, Node* pre, string data);
+	void deleteNode(DblLinkedList* L, Node* old);
 };
 DblLinkedList::DblLinkedList() {
 	head = NULL;
 }
-DblNode* DblLinkedList::getHead() {
+Node* DblLinkedList::getHead() {
 	return head;
 }
-void DblLinkedList::insertNode(DblLinkedList* L, DblNode* pre, string data){
-	DblNode newNode(data);
-	if (L->head == NULL) {
-		L->head = &newNode;
+void DblLinkedList::setHead(Node* willhead) {
+	head = willhead;
+}
+void DblLinkedList::insertNode(DblLinkedList* L, Node* pre, string data){
+	Node* newNode = new Node(data);
+	newNode->data = data;
+	if (L->getHead() == NULL) {
+		L->setHead(newNode);
 	}
 	else {
-		newNode.rlink = pre->rlink;
-		pre->rlink = &newNode;
-		newNode.llink = pre;
-		if (newNode.rlink != NULL){
-			DblNode* post = newNode.rlink;
-			post->llink = &newNode;
-			post->rlink = NULL;
+		newNode->rlink = pre->rlink;
+		pre->rlink = newNode;
+		newNode->llink = pre;
+		if (newNode->rlink != NULL){
+			Node* post = newNode->rlink;
+			post->llink = newNode;
 		}
 
 	}
 }
-void DblLinkedList::deleteNode(DblLinkedList* L, DblNode* old) {
+void DblLinkedList::deleteNode(DblLinkedList* L, Node* old) {
 	if (L->head == old) {
 		L->head = NULL;
 	}
 	else {
-		DblNode* pre = old->llink;
-		DblNode* post = old->rlink;
-		old->llink = post->llink;
-		pre->rlink = old->rlink;
+		Node* pre = old->llink;
+		Node* post = old->rlink;
+		if (post!=NULL) {
+			pre->rlink = post;
+			post->llink = pre;
+		}
 	}
 }
 int main() {
 	DblLinkedList* L = new DblLinkedList();
-	DblNode* pre = NULL;
+	
+	Node* pre = NULL;
 	L->insertNode(L, pre, "월");
 
-	DblNode* mon = (L->getHead());
-	L->insertNode(L, mon, "화");	
-	DblNode* tue = mon->rlink;
-	L->insertNode(L, tue, "수");
-	DblNode* wed = tue->rlink;
-	L->insertNode(L, wed, "목");
-	DblNode* thu = wed->rlink;
-	L->insertNode(L, thu, "금");
-	DblNode* fri = thu->rlink;
-	L->insertNode(L, fri, "토");
-	DblNode* sat = fri->rlink;
-
-	DblNode* temp = L->getHead();
+	Node* mon = L->getHead();
 	
+	L->insertNode(L, mon, "목");
+	
+	Node* thu = mon->rlink;
+	L->insertNode(L, thu, "금");
+	
+	Node* fri = thu->rlink;
+	L->insertNode(L, fri, "토");
+	//////////////////////////
+	Node* sat = fri->rlink;
+
+	L->insertNode(L, mon, "화");
+
+	Node* tue = mon->rlink;
+	L->insertNode(L, tue, "수");
+
+	Node* wed = tue->rlink;
+
+	Node* temp = L->getHead();
+
 	cout << "Double Linked List L = (";
 	while (temp != NULL) {
-		cout << temp->getData();
-		pre = temp; temp = temp->rlink;
+		cout << temp->data;
+		temp = temp->rlink;
 		if (temp != NULL) {
 			cout << ", ";
 		}
 	}
 	cout << ")" << endl;
 
+	L->deleteNode(L, thu);
+	L->deleteNode(L, sat);
+	
+	cout << "Double Linked List Reverse L = (";
+	temp = fri;
+	while (temp != NULL) {
+		cout << temp->data;
+		temp = temp->llink;
+		if (temp != NULL)
+			cout << ", ";
+	}
+	cout << ")" << endl;
 
 }
